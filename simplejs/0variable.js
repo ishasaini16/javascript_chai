f1fe04d3a863a6302f9c2f679e28d2e4082a7d70
ishasaini16 <80397386+ishasaini16@users.noreply.github.com>
2024-02-29 11:54:30 +0000

    varibales

---
+++ simplejs/0variable.js
@@ -0,0 +1,14 @@
+const accountId =1445
+let email="developerit37@gmail.com"
+var password="12345"
+accountCity="jaipur"
+email="test@gmail.com"
+password="vhsdgbgh"
+accountCity="delhi"
+//accountId=3
+
+console.log(accountId);
+
+console.table([accountId,accountCity,email,password])
+
+//data types
